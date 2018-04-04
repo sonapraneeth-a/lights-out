635185b747a58eb47161794a6e9f60c8dd29f478
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-04-04 22:07:33 +0530

    Updated all the files for the repository

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Sona Praneeth Akula - Home page',
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
 import Game from './board/Game';
 
+/*https://github.com/gatsbyjs/gatsby/issues/1846 */
 export default () => <Game title="Lights Out" version="v0.11.1" debug="yes"/>
